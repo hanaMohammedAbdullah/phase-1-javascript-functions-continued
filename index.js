6c12bf42e26bc6aed3c5edc5f5a908d58f3b0ffe
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-02-29 16:45:43 +0300

    half  Assigmmnet

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(arg) {
+
+  return arg ? `This Saturday, I want to ${arg}!` : "This Saturday, I want to roller-skate!";
+}
+
+(function () {
+  console.log("Yet more razzling");
+});
+
+
+const mondayWork = function (work){
+return work ? `This Monday, I will ${work}.` : `This Monday, I will go to the office.`
+}
+

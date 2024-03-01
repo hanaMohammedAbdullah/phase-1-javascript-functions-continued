e3a87cb128590ae19c6eabba90d9c7a34522c091
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-01 15:00:29 +0300

    complete  Assigmmnet

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // code your solution here
 function saturdayFun(arg) {
-
-  return arg ? `This Saturday, I want to ${arg}!` : "This Saturday, I want to roller-skate!";
+  return arg
+    ? `This Saturday, I want to ${arg}!`
+    : "This Saturday, I want to roller-skate!";
 }
 
 (function () {
   console.log("Yet more razzling");
 });
 
+const mondayWork = function (work) {
+  return work
+    ? `This Monday, I will ${work}.`
+    : `This Monday, I will go to the office.`;
+};
 
-const mondayWork = function (work){
-return work ? `This Monday, I will ${work}.` : `This Monday, I will go to the office.`
-}
+const wrapAdjective = function wrapAdjective(arg = "!!!") {
+  return function (end) {
+    return `You are ${arg}${end}${arg}!`;
+  };
+};
 
+wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
